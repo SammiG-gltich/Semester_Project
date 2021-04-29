a0b773643857ab57e9b37c860be762af94017154
SammiG <gervse@farmingdale.edu>
2021-04-28 20:32:53 -0400

    Playing cards basic set up

---
+++ SemesterProject/src/SemesterProjectController.java
@@ -0,0 +1,49 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class SemesterProjectController {
+
+    @FXML
+    private Button solutionBtn;
+
+    @FXML
+    private Button checkBtn;
+
+    @FXML
+    private TextField txt1;
+
+    @FXML
+    private TextField txt2;
+
+    @FXML
+    private Button clearBtn;
+
+    @FXML
+    void findSol(ActionEvent event) {
+
+    }
+
+    @FXML
+    void houseKeeping(ActionEvent event) {
+
+    }
+
+    @FXML
+    void makeSure(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showMe(ActionEvent event) {
+
+    }
+
+    @FXML
+    void userIn(ActionEvent event) {
+
+    }
+
+}
+
