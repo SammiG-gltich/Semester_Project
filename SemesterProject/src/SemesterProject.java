e4c16a01c77fb26a45d6fbd406715f4fe7d1bcb0
SammiG <gervse@farmingdale.edu>
2021-05-09 22:57:55 -0400

    makeSure function brings txt to dialog if equal to 24(need checks for operands)

--- SemesterProject/src/SemesterProject.java
+++ SemesterProject/src/SemesterProject.java
@@ -7,9 +7,13 @@
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -18,26 +22,34 @@ import javafx.stage.Stage;
  */
 public class SemesterProject extends Application{
 
+    Image firstCard; 
+    Image secondCard; 
+    Image thirdCard; 
+    Image fourthCard;
+    ImageView fCard;
+    ImageView sCard;
+    ImageView tCard;
+    ImageView foCard;
     
     public static void main(String[] args) {
      launch(args);
     }
     //display a tutorial document on start
     
-    //display 4 random images on start.
-    
-    
-
     @Override
     public void start(Stage primaryStage) throws Exception {
-        
+
     Parent gp= FXMLLoader.load(getClass().getResource("SemesterProject.fxml"));
-
+    
+    
+            
     primaryStage.setScene( new Scene(gp));
-
+    
     primaryStage.show();
     
     
     }
     
 }
+
+
